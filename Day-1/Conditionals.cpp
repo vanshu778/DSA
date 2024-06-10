a0a5ed3497c43aa844d644ff64072807be3b9e71
Vanshika Thesiya <101634231+Vanshika200003@users.noreply.github.com>
2024-06-10 16:16:23 +0530

    Conditionals.cpp

---
+++ Day-1/Conditionals.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // int score;
+  // cout<<"Enter score: "<<endl;
+  // cin>>score;
+  // if(score<300){
+  //   cout<<"India wins"<<endl;
+  // }
+  // else{
+  //   cout<<"Pakistan wins"<<endl;
+  // }
+
+  // int marks;
+  // cin>>marks;
+
+  // if(marks>=90){
+  //   cout<<"A grade";
+  // }
+  //  else{
+  //    if(marks>=80){
+  //      cout<<"B grade";
+  //    }
+  //    else{
+  //      if(marks>=60){
+  //        cout<<"C grade";
+  //      }
+  //      else{
+  //        if(marks>=40){
+  //          cout<<"D grade";
+  //        }
+  //        else{
+  //          cout<<"F grade";
+  //        }
+  //      }
+  //    }
+  //  }
+  // if else-if
+  // if(marks>=90){
+  //   cout<<"A grade";
+  // }
+  // else if(marks>=80){
+  //     cout<<"B grade";
+  //   }
+  // else if(marks>=60){
+  //         cout<<"C grade";
+  //       }
+  // else if(marks>=40){
+  //   cout<<"D grade";
+  // }
+  // else{
+  //   cout<<"F grade";
+  // }
+
+
+}
