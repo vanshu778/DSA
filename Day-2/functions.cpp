e09c561594b1aae7a34b4678167fe71e75b6cebd
Vanshika Thesiya <101634231+Vanshika200003@users.noreply.github.com>
2024-06-10 16:15:32 +0530

    functions.cpp

---
+++ Day-2/functions.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+using namespace std;
+
+// void printName(){
+//   int n;
+//   cout<<"Enter the value of n:"<< endl;
+//   cin>>n;
+
+//   for(int i=0;i<n;i++){
+//     cout<<"Vanshika"<<endl;
+//   }
+// }
+// int main() {
+  //printName();
+// void printNumber(int num){
+//   cout<<num<<endl;
+  // }
+
+
+
+// void printNumber(int num){
+//   cout<<num<<endl;
+// }
+
+// int main() {
+   // int a=5;
+  // printNumber(a);
+// }
+
+// Add 2 numbers
+// int add(int a, int b){
+//   int result=a+b;
+//   return result;
+// }
+
+// int main(){
+//   int a,b;
+//   cout<<"Enter the value of a:"<<endl;
+//   cin>>a;
+  
+//   cout<<"Enter the value of b:"<<endl;
+//   cin>>b;
+
+//   int sum = add(a,b);
+//   cout<<"Addition result is:"<<sum<<endl;
+
+//   return 0;
+// }
+
+//find max 3 numbers
+// int findmax(int a,int b, int c){
+//   if(a>b && a>c){
+//     return a;
+//   }
+//   else if(b>a && b>c){
+//     return b;
+//   }
+//   else{
+//     return c;
+//   }
+// }
+// int main(){
+//   int a,b,c;
+// cin >> a >> b >> c;
+//   int maxNumber=findmax(a,b,c);
+//   cout<<maxNumber<<endl;
+// }
+
+//counting 1 to N
+
+// void printCounting(int n){
+//   for(int i=1;i<=n;i++){
+//     cout<<i<<" ";
+//   }
+//   cout<<endl;
+// }
+// int main(){
+//   int n;
+//   cout<<"Enter the value n:"<<endl;
+//   cin>>n;
+
+//   printCounting(n);
+// }
+
+//function of Students and Grade problem
+// char getgrade(int marks){
+//   if(marks>=90)
+//     return 'A';
+//   else if(marks>=80)
+//     return 'B';
+//   else if(marks>=70)
+//     return 'C';
+//   else if(marks>=60)
+//     return 'D';
+//   else
+//     return 'F';
+// }
+// int main(){
+//   int marks;
+//   cout<<"Enter the marks:"<<endl;
+//   cin>>marks;
+
+//   char finalGrade=getgrade(marks);
+//   cout<<finalGrade<<endl;
+// }
+
+// Sum upto N
+
+// int getSum(int n){
+//   int sum = 0;
+//   for(int i=0;i<=n;i++){
+//     sum=sum+i;
+//   }
+//   return sum;
+// }
+// int main(){
+//   int n;
+//   cout<<"Enter the value of n:"<<endl;
+//   cin>>n;
+
+//   int ans=getSum(n);
+//   cout<<"Sum upto "<<n<<"is "<<ans<<endl;
+//   return 0;
+// }
+
+// Sum of Even number upto N
+// int getEvenSum(int n){
+//   int sum = 0;
+//   for(int i=2;i<=n;i+=2){
+//     sum=sum+i;
+//   }
+//   return sum;
+// }
+// int main(){
+//   int n;
+//   cout<<"Enter the value of n:"<<endl;
+//   cin>>n;
+
+//   int ans=getEvenSum(n);
+//   cout<<"Sum upto "<<n<<"is "<<ans<<endl;
+//   return 0;
+// }
