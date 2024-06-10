bd90e760265c9e30c88ee39ad18aa7dc6c1d2e09
Vanshika Thesiya <101634231+Vanshika200003@users.noreply.github.com>
2024-06-10 16:05:47 +0530

    Add files via upload

---
+++ Day-2/operators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // bool a=false;
+  // bool b=true;
+
+  // cout<<(a|b);
+  // cout << ~(a);
+  // cout<<(a^b);
+
+  // cout<<(2 & 3) <<endl;
+
+  // int a = 12;
+  // a = a << 1;
+  // cout << a << endl;
+
+  // int a = 12;
+  // a = a >> 1;
+  // cout << a << endl;
+
+  // int a=11;
+  // cout<<++a;
+  // cout<<a++;
+  // cout<<a;
+
+  int a=5;
+  cout<<(++a)*(++a);
+}
