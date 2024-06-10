b2cf4ed683b4cd998c6fc1b4320f5a20ce904a66
Vanshika Thesiya <101634231+Vanshika200003@users.noreply.github.com>
2024-06-10 16:16:55 +0530

    Update Loops.cpp

---
+++ Day-1/Loops.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // for(int i=0;i<5;i=i+1){
+  //   cout<<i<<endl;
+  // }
+
+  // for(int i=5;i>0;i=i-1){
+  //   cout<<i<<endl;
+  // }
+
+  // for(int i=1;i<=10;i=i+1){
+  //   cout<<2*i<<endl;
+  // }
+  // for(int i=0;i<=5;i=i+2){
+  //   cout<<i<<endl;
+  // }
+
+  // for(int i=1;i<=5;i=i+2){
+  //   cout<<i<<endl;
+  // }
+  // for(int i=1;i<10;i=i*2){
+  //   cout<<i<<endl;
+  // }
+  // for(int i=100;i>0;i=i/2){
+  //   cout<<i<<endl;
+  // }
+  // for(int i=5;(i>=0 && i<=10);i=i+1){
+  //   cout<<i<<endl;
+  // }
+}
